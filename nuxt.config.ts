5f1c5f02b4ec6a77dacfe148161a464561377259
caoyang2002 <cy12968@163.com>
2025-10-30 14:02:22 +0800

    update pnpm c

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,22 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   ssr:false,
-
+ app: {
+    baseURL: '/SimpleResume/',
+    head: {
+      title: 'Simple Resume Builder',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'A simple and beautiful resume builder' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/SimpleResume/favicon.ico' },
+        { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css' }
+      ]
+    },
+    buildAssetsDir: 'assets'
+  },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   css: ['../assets/css/main.css'],
@@ -16,7 +31,17 @@ export default defineNuxtConfig({
   experimental: {
     clientFallback: true
   },
+   components: true,
+   srcDir: './pages',
+    // 路由配置
+  routeRules: {
+    '/': { prerender: true },
+    '/**': { prerender: false }
+  },
    nitro: {
-    preset: 'github-pages'
+    preset: 'github-pages',
+     prerender: {
+      routes: ['/']
+    }
   }
 })
